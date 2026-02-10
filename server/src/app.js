36814c60f619ab426c240b0857d7549a560b9bcf
David Mani Ibrahim <ibrahimmanidavid@gmail.com>
2026-02-10 07:26:32 +0100

    feat: implement initial full-stack Valentine application with React/Vite client, Express server, and project README.

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import valentineRoutes from "./routes/valentineRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
@@ -14,5 +16,17 @@ app.use(cors({
 
 app.use(express.json());
 app.use("/api/valentine", valentineRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../../valantine/dist")));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../valantine/dist/index.html"));
+});
 
 export default app;
